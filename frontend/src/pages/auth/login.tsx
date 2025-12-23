aaa3e5eef2e8d952dfa70e80b542703af74c17f4
unknown <caiolopesfv@gmail.com>
2025-12-22 21:24:33 -0300

    Iniciando tela de login e registro

---
+++ frontend/src/pages/auth/login.tsx
@@ -0,0 +1,5 @@
+const Login = () => {
+  return <div className="">Login Page</div>;
+};
+
+export default Login;
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -3,20 +3,44 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 //pages
 import Home from "./pages/home";
 import ProductPage from "./pages/product/product";
+import Login from "./pages/auth/login";
+import Register from "./pages/auth/register";
 
 //components
 import Header from "./components/molecules/header/header";
 import Footer from "./components/atoms/footer/footer";
 
+const DefaultLayout = ({ children }: { children: React.ReactNode }) => (
+  <>
+    <Header />
+    {children}
+    <Footer />
+  </>
+);
+
 const Router = () => {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product/:id" element={<ProductPage />} />
+        <Route
+          path="/"
+          element={
+            <DefaultLayout>
+              <Home />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path="/product/:id"
+          element={
+            <DefaultLayout>
+              <ProductPage />
+            </DefaultLayout>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 };
