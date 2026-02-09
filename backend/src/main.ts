a6623bfc1476d35f81dffea343c5f4ae2a856832
unknown <caiolopesfv@gmail.com>
2026-02-09 20:29:18 -0300

    Atualizando main para exportar servidor expresse corretamente para a vercel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,18 +2,30 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import serverless from 'serverless-http';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+const isServerless = !!process.env.VERCEL;
+let cachedServer: any;
+
+async function createApp(expressApp?: any) {
+  let app;
+
+  if (expressApp) {
+    app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+  } else {
+    app = await NestFactory.create(AppModule);
+  }
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     }),
   );
-  app.enableCors({
-    origin: '*',
-  });
+
+  app.enableCors({ origin: '*' });
 
   const configSwagger = new DocumentBuilder()
     .setTitle('Buki API')
@@ -21,11 +33,31 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
 
-  const documentFactory = () =>
-    SwaggerModule.createDocument(app, configSwagger);
+  const document = SwaggerModule.createDocument(app, configSwagger);
 
-  SwaggerModule.setup('/api/swagger', app, documentFactory);
+  if (expressApp) {
+    SwaggerModule.setup('/api/swagger', expressApp, document);
+  } else {
+    SwaggerModule.setup('/api/swagger', app, document);
+  }
 
-  await app.listen(process.env.PORT ?? 3000);
+  return app;
 }
-bootstrap();
+
+async function bootstrap() {
+  const app = await createApp();
+  await app.listen(3000);
+}
+
+export const handler = async (req, res) => {
+  if (!cachedServer) {
+    const expressApp = express();
+    await createApp(expressApp);
+    cachedServer = serverless(expressApp);
+  }
+  return cachedServer(req, res);
+};
+
+if (!isServerless) {
+  bootstrap();
+}
